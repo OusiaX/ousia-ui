bbd7208a6d8cc7e69c6ca30155268023d0696547
ousiastx <ousiastx@gmail.com>
2025-11-02 06:34:17 +0900

    Feat: ark factory with React 19 Ref

--- packages/ark/src/factory.ts
+++ packages/ark/src/factory.ts
@@ -1,5 +1,3 @@
-// Do not replace with '@zag-js/react'
-import { mergeProps } from '@zag-js/core'
 import type React from 'react'
 import {
   Children,
@@ -7,10 +5,9 @@ import {
   type JSX,
   cloneElement,
   createElement,
-  forwardRef,
   isValidElement,
-  memo,
 } from 'react'
+import { mergeProps } from './merge-props'
 import { composeRefs } from './utils/compose-refs'
 
 export interface PolymorphicProps {
@@ -20,66 +17,43 @@ export interface PolymorphicProps {
   asChild?: boolean | undefined
 }
 
-type IntrinsicElementProps<T extends keyof JSX.IntrinsicElements> = JSX.IntrinsicElements[T]
-type ArkComponent<T extends keyof JSX.IntrinsicElements> = React.ForwardRefExoticComponent<
-  IntrinsicElementProps<T> & PolymorphicProps & React.RefAttributes<any>
->
-type SimpleComponent<T extends keyof JSX.IntrinsicElements> = React.ForwardRefExoticComponent<
-  IntrinsicElementProps<T> & React.RefAttributes<any>
->
-
 type JsxElements = { [E in keyof JSX.IntrinsicElements]: ArkComponent<E> }
-type SimpleElements = { [E in keyof JSX.IntrinsicElements]: SimpleComponent<E> }
+type ArkComponent<T extends keyof JSX.IntrinsicElements> = React.FC<
+  JSX.IntrinsicElements[T] & PolymorphicProps & { ref?: React.Ref<unknown> }
+>
+type ArkPropsWithRef<E extends React.ElementType> = React.ComponentPropsWithRef<E> &
+  PolymorphicProps
 
-type ArkPropsWithRef<E extends React.ElementType> = React.ComponentPropsWithRef<E> & PolymorphicProps
-
-// Credits to the Radix team
-function getRef(element: React.ReactElement) {
-  type ElementWithRef = React.ReactElement & { ref?: React.Ref<unknown> }
-  type PropsWithRef = { ref?: React.Ref<unknown> }
-
-  // React <=18 in DEV
-  let getter = Object.getOwnPropertyDescriptor(element.props, 'ref')?.get
-  let mayWarn = getter && 'isReactWarning' in getter && getter.isReactWarning
-  if (mayWarn) {
-    return (element as ElementWithRef).ref
-  }
-
-  // React 19 in DEV
-  getter = Object.getOwnPropertyDescriptor(element, 'ref')?.get
-  mayWarn = getter && 'isReactWarning' in getter && getter.isReactWarning
-  if (mayWarn) {
-    return (element.props as PropsWithRef).ref
-  }
-
-  return (element.props as PropsWithRef).ref || (element as ElementWithRef).ref
+// In React 19, ref is always a regular prop
+function getRef<T = unknown>(element: React.ReactElement) {
+  return (element.props as { ref?: React.Ref<T> }).ref
 }
 
-const withAsChild = (Component: React.ElementType, shouldMemoize = false) => {
-  const ComponentImpl = forwardRef<unknown, ArkPropsWithRef<typeof Component>>((props, ref) => {
-    if (!props['asChild']) {
-      return createElement(Component, { ...props, ref }, props['children'])
+const withAsChild = (Component: React.ElementType) => {
+  const Comp = (props: ArkPropsWithRef<typeof Component>) => {
+    if (!props.asChild) {
+      return createElement(Component, props)
     }
 
-    const { asChild, children, ...restProps } = props
+    const { asChild, children, ref, ...restProps } = props
 
     if (!isValidElement<Record<string, unknown>>(children)) {
       return null
     }
 
     const onlyChild: React.ReactElement<Record<string, unknown>> = Children.only(children)
+
     const childRef = getRef(onlyChild)
+    const { ref: _, ...childPropsWithoutRef } = onlyChild.props
 
     return cloneElement(onlyChild, {
-      ...mergeProps(restProps, onlyChild.props),
+      ...mergeProps(restProps, childPropsWithoutRef),
       ref: ref ? composeRefs(ref, childRef) : childRef,
     })
-  })
+  }
 
-  const Comp = shouldMemoize ? memo(ComponentImpl) : ComponentImpl
-
-  const componentWithName = Component as { displayName?: string; name?: string }
-  Comp.displayName = componentWithName.displayName || componentWithName.name || 'Component'
+  // @ts-expect-error - it exists
+  Comp.displayName = Component.displayName || Component.name
 
   return Comp
 }
@@ -87,62 +61,21 @@ const withAsChild = (Component: React.ElementType, shouldMemoize = false) => {
 export type HTMLProps<T extends keyof JSX.IntrinsicElements> = ComponentPropsWithoutRef<T>
 export type HTMLArkProps<T extends keyof JSX.IntrinsicElements> = HTMLProps<T> & PolymorphicProps
 
-const createSimpleComponent = (Component: React.ElementType) => {
-  const Comp = forwardRef<unknown, React.ComponentPropsWithRef<typeof Component>>((props, ref) => {
-    return createElement(Component, { ...props, ref })
-  })
+export const jsxFactory = () => {
+  const cache = new Map()
 
-  const componentWithName = Component as { displayName?: string; name?: string }
-  Comp.displayName = componentWithName.displayName || componentWithName.name || 'Component'
-
-  return Comp
-}
-
-export const jsxFactory = (): JsxElements => {
   return new Proxy(withAsChild, {
-    apply(_target, _thisArg, argArray: [React.ElementType]) {
-      return withAsChild(argArray[0], false)
+    apply(_target, _thisArg, argArray) {
+      return withAsChild(argArray[0])
     },
-    get(_, element: string | symbol) {
-      if (typeof element === 'string') {
-        const asElement = element as keyof JSX.IntrinsicElements
-        return withAsChild(asElement, false)
+    get(_, element) {
+      const asElement = element as React.ElementType
+      if (!cache.has(asElement)) {
+        cache.set(asElement, withAsChild(asElement))
       }
-      return undefined
-    },
-  }) as unknown as JsxElements
-}
-
-export const simpleFactory = (): SimpleElements => {
-  return new Proxy(createSimpleComponent, {
-    apply(_target, _thisArg, argArray: [React.ElementType]) {
-      return createSimpleComponent(argArray[0])
-    },
-    get(_, element: string | symbol) {
-      if (typeof element === 'string') {
-        const asElement = element as keyof JSX.IntrinsicElements
-        return createSimpleComponent(asElement)
-      }
-      return undefined
-    },
-  }) as unknown as SimpleElements
-}
-
-export const memoizedFactory = (): JsxElements => {
-  return new Proxy(withAsChild, {
-    apply(_target, _thisArg, argArray: [React.ElementType]) {
-      return withAsChild(argArray[0], true)
-    },
-    get(_, element: string | symbol) {
-      if (typeof element === 'string') {
-        const asElement = element as keyof JSX.IntrinsicElements
-        return withAsChild(asElement, true)
-      }
-      return undefined
+      return cache.get(asElement)
     },
   }) as unknown as JsxElements
 }
 
 export const ark = jsxFactory()
-export const arkSimple = simpleFactory()
-export const arkMemo = memoizedFactory()
--- packages/ark/src/index.ts
+++ packages/ark/src/index.ts
@@ -1,9 +1,5 @@
-export type { Assign, Optional } from "./types";
+export type { Assign, Optional } from './types'
+export type { HTMLArkProps, HTMLProps, PolymorphicProps } from './factory'
 
-export type { HTMLArkProps, HTMLProps, PolymorphicProps } from "./factory";
-
-export {
-  ark,
-  arkMemo,
-  arkSimple,
-} from "./factory";
+export { ark } from './factory'
+export { mergeProps } from './merge-props'
