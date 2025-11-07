24656988104dee6ea7c4f5f7acd0bf2b11b83294
ousiastx <ousiastx@gmail.com>
2025-11-07 21:57:16 +0900

    Misc: Update recipes index.ts

--- components/src/theme/recipes/index.ts
+++ components/src/theme/recipes/index.ts
@@ -1,23 +1,30 @@
-import type { RecipeConfig, RecipeVariantRecord } from "@pandacss/dev";
-import { badge } from "./badge";
-import { button } from "./button";
-import { formLabel } from "./form-label";
-import { icon } from "./icon";
-import { input } from "./input";
-import { skeleton } from "./skeleton";
-import { spinner } from "./spinner";
-import { textarea } from "./textarea";
+import type { RecipeConfig, RecipeVariantRecord } from '@pandacss/dev'
+import { badge } from './badge'
+import { button } from './button'
+import { code } from './code'
+import { formLabel } from './form-label'
+import { heading } from './heading'
+import { icon } from './icon'
+import { input } from './input'
+import { kbd } from './kbd'
+import { link } from './link'
+import { skeleton } from './skeleton'
+import { spinner } from './spinner'
+import { text } from './text'
+import { textarea } from './textarea'
 
-export const recipes: Record<
-  string,
-  Partial<RecipeConfig<RecipeVariantRecord>>
-> = {
+export const recipes: Record<string, Partial<RecipeConfig<RecipeVariantRecord>>> = {
   badge,
   button,
+  code,
   formLabel,
+  heading,
   icon,
   input,
+  kbd,
+  link,
   skeleton,
   spinner,
+  text,
   textarea,
-};
+}
